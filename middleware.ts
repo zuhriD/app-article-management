0a53a24ee30aacff7c5ec72538ea549d5a145c19
zuhriD <dikizuhri@gmail.com>
2024-10-14 22:14:28 +0700

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,29 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
-
-export function middleware(request: NextRequest) {
-    // Check if the user is authenticated by checking for 'authjs.session-token'
-    const isAuthenticated = !!request.cookies.get('__Secure-authjs.session-token');
-
-    console.log('isAuthenticated:', isAuthenticated);
-
-    // If the user is not authenticated and is not on the sign-in page, redirect to /auth/signin
-    if (!isAuthenticated && request.nextUrl.pathname !== '/auth/signin') {
-        return NextResponse.redirect(new URL('/auth/signin', request.url));
-    }
-
-    return NextResponse.next(); // Allow the request to proceed
-}
+export { auth as middleware } from './auth';
 
 export const config = {
-    matcher: [
-        // Match all routes except sign-in
-        '/((?!api|_next/static|_next/image|auth/signin).*)'
-    ],
-};
+    matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+}
+
+// import { NextResponse } from 'next/server';
+// import type { NextRequest } from 'next/server';
+
+// export function middleware(request: NextRequest) {
+//     // Check if the user is authenticated by checking for 'authjs.session-token'
+//     const isAuthenticated = !!request.cookies.get('__Secure-authjs.session-token');
+
+//     console.log('isAuthenticated:', isAuthenticated);
+
+//     // If the user is not authenticated and is not on the sign-in page, redirect to /auth/signin
+//     if (!isAuthenticated && request.nextUrl.pathname !== '/auth/signin') {
+//         return NextResponse.redirect(new URL('/auth/signin', request.url));
+//     }
+
+//     return NextResponse.next(); // Allow the request to proceed
+// }
+
+// export const config = {
+//     matcher: [
+//         // Match all routes except sign-in
+//         '/((?!api|_next/static|_next/image|auth/signin).*)'
+//     ],
+// };
